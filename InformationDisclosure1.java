c916a23f423ac08bf5e5a944e83f48eb2c0b7358
husler20 <145982298+husler20@users.noreply.github.com>
2024-07-17 14:41:44 +0800

    Create InformationDisclosure1.java

---
+++ InformationDisclosure1.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // 假设我们有一个简单的用户认证机制
+        if ("admin".equals(username) && "password123".equals(password)) {
+            response.getWriter().println("Login successful");
+        } else {
+            // 详细的错误信息可能会泄露信息
+            response.getWriter().println("Login failed: Invalid username or password");
+        }
+    }
+}
